63278caa3e5b7713c0785d6a2381373c914d567a
Sugali Abhishek <sugali.alnaikk.ece17@itbhu.ac.in>
2022-01-27 00:14:39 +0530

    v2

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -7,6 +7,8 @@ import { Col, Row, Typography, Select, Spin} from 'antd';
 import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../services/cryptoApi';
 import LineChart from './LineChart';
 
+
+
 const { Title}= Typography;
 const { Option } = Select;
 
@@ -35,21 +37,7 @@ const CryptoDetails = () => {
   console.log("1",cryptoDetails);
 
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
-
-{/*const stats = [
-    { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
-    { title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
-    { title: 'Market Cap', value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined /> },
-    { title: 'All-time-high(daily avg.)', value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`, icon: <TrophyOutlined /> },
-  ];
   
-  const genericStats = [
-    { title: 'Number Of Markets', value: cryptoDetails.numberOfMarkets, icon: <FundOutlined /> },
-    { title: 'Number Of Exchanges', value: cryptoDetails.numberOfExchanges, icon: <MoneyCollectOutlined /> },
-    { title: 'Aprroved Supply', value: cryptoDetails.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
-    { title: 'Total Supply', value: `$ ${millify(cryptoDetails.totalSupply)}`, icon: <ExclamationCircleOutlined /> },
-    { title: 'Circulating Supply', value: `$ ${millify(cryptoDetails.circulatingSupply)}`, icon: <ExclamationCircleOutlined /> },
-  ];*/}
     return (  
     <div style={cssdetails}>
         <Col style={{margin:'0 2rem 0'}}>
